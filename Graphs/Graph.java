2fd63ab96d76f3fa12467f7a3f6575bd95d3f573
farkh <farkhutdinov1398@gmail.com>
2016-02-09 23:47:17 +0300

    First version of Graphs

---
+++ Graphs/Graph.java
@@ -0,0 +1,5 @@
+public interface Graph {
+  void addVertex();
+  void addEdge(int a, int b);
+  void showGraph();
+}
---
+++ Graphs/Main.java
@@ -0,0 +1,19 @@
+class Main {
+  public static void main(String[] args) {
+    Graph graph = new Matrix();
+    
+    graph.addVertex();
+    graph.addVertex();
+    graph.addVertex();
+    graph.addVertex();
+    graph.addVertex();
+
+    graph.addEdge(0, 2);
+    graph.addEdge(0, 3);
+    graph.addEdge(3, 4);
+    graph.addEdge(2, 1);
+    graph.addEdge(2, 4);
+
+    graph.showGraph();
+  }
+}
